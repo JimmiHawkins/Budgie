db97c9108ca07eaa81171f7566dd6fb9bfff625c
JimmiHawkins <79973489+JimmiHawkins@users.noreply.github.com>
2022-05-20 04:53:26 +0100

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,16 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDcIAs0OKb_oViDAAnDFLHy7E7XfNz1iIk",
-  authDomain: "budgie-web-app.firebaseapp.com",
-  projectId: "budgie-web-app",
-  storageBucket: "budgie-web-app.appspot.com",
-  messagingSenderId: "747088624792",
-  appId: "1:747088624792:web:3994192943bc76ab7f64ed",
-  measurementId: "G-X9FPLB2EJL"
+  apiKey: "AIzaSyDMF3dVQ-vOsSfWl5EyaQSL9vpr4tZk9do",
+  authDomain: "budgie-b49e1.firebaseapp.com",
+  projectId: "budgie-b49e1",
+  storageBucket: "budgie-b49e1.appspot.com",
+  messagingSenderId: "418162914612",
+  appId: "1:418162914612:web:2127f170268ac118bc6161",
+  measurementId: "G-R7N2Z97Y72"
   };
 
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
+
+
 
 
--- js/script.js
+++ js/script.js
@@ -1,26 +1,48 @@
 var loginForm = document.querySelector('#loginForm');
 
-loginForm.addEventListener('.submit',(e)=>{
-    e.preventDefault();
-    
-    db.collection('loginForm').doc().set({
-        username: loginForm.uname.value,
-        password: loginForm.psw.value,
+loginForm.addEventListener('submit', (e) => {
+	e.preventDefault();
 
-    }).then(()=>{
-        
-    })   
+	db.collection('details').doc().set({
+		username: loginForm.uname.value,
+		password: loginForm.psw.value
 
-    if (loginForm.uname.value === "1" && loginForm.psw.value === "1") {
-        
-        console.log('hi');
-        
-    } else {
-        alert("Invalid information");
-        console.log(loginForm.uname.value);
-        
-    }})
+	}).then(() => {
+		if (loginForm.uname.value === "12345" && loginForm.psw.value === "12345") {
+
+			location.href = "home.html";
+
+		} else {
+			alert("Invalid information");
+		}
+
+	})
 
 
+});
+
+console.log("js")
+// const nameList = document.querySelector('#name-user');
 
 
+// function renderName(doc){
+//     let li = document.createElement('li');
+//     let n = document.createElement('span');
+//     let u = document.createElement('span');
+
+//     li.setAttribute('data-id', doc.id);
+//     n.textContent = doc.data().n;
+//     u.textContent = doc.data().u;
+
+//     li.appendChild(n);
+//     li.appendChild(u);
+
+//     nameList.appendChild(li);
+// }
+
+
+// db.collection('details').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderName(doc);
+//     });
+// });
